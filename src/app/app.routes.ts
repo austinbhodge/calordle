8fb1d93321ae35c35dcf5a058ce5d6805c13f4c4
Austin Hodge <austinbhodge@gmail.com>
2024-06-28 23:12:07 -0400

    Adds prototype of the game

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { TodayComponent } from './pages/today/today.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent},
+  { path: 'today', component: TodayComponent}
 ];
