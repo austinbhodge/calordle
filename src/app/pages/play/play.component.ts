537c83358a76d977ccab6ead25290231906a719d
Austin Hodge <austinbhodge@gmail.com>
2024-08-14 21:23:28 -0400

    adds favicon and logo, removes random food order, better formatting.

--- src/app/pages/play/play.component.ts
+++ src/app/pages/play/play.component.ts
@@ -9,6 +9,7 @@ interface FoodItem {
   name: string;
   calories: number;
   imageURI: string;
+  sourceURI: string;
   protein: number;
   carb: number;
   fat: number;
@@ -29,6 +30,7 @@ interface FoodItem {
 export class PlayComponent {
   foodItems: FoodItem[] = foodData;
   currentFood: FoodItem | null = null;
+  currentFoodIndex: number = -1;
   userGuess: number = 0;
   feedback: string = '';
   guessesLeft: number = 3;
@@ -38,12 +40,15 @@ export class PlayComponent {
   gameWon: boolean = false;
 
   ngOnInit() {
-    this.selectRandomFood();
+    this.selectNextFood();
   }
 
-  selectRandomFood() {
-    const randomIndex = Math.floor(Math.random() * this.foodItems.length);
-    this.currentFood = this.foodItems[randomIndex];
+  selectNextFood() {
+    this.currentFoodIndex += 1
+    if (this.currentFoodIndex >= this.foodItems.length) {
+      this.currentFoodIndex = 0;
+    }
+    this.currentFood = this.foodItems[this.currentFoodIndex];
     this.userGuess = 0;
     this.feedback = '';
     this.guessesLeft = 3;
@@ -77,10 +82,10 @@ export class PlayComponent {
     if (this.guessesLeft > 0 && !this.gameOver) {
       this.revealLowestMacro();
     } else if (this.gameWon) {
-      this.feedback = `Good Job! The exact answer is approximately ${this.currentFood.calories} calories.`;
+      this.feedback = `Good Job! The answer is approximately ${this.currentFood.calories} calories.`;
     } else {
       this.gameOver = true;
-      this.feedback = `Game over. The correct answer was ${this.currentFood.calories} calories.`;
+      this.feedback = `Sorry! The correct answer is ${this.currentFood.calories} calories.`;
     }
 
     this.userGuess = 0;
@@ -96,7 +101,7 @@ export class PlayComponent {
     ];
 
     macros.sort((a, b) => a.value - b.value);
-    this.revealedMacro = `${macros[0].name}: ${macros[0].value}g`;
+    this.revealedMacro = `${macros[0].name === 'fat' ? macros[0].value * 9 : macros[0].value * 4 } calories from ${macros[0].name}s`;
   }
 
   createRange(number: number){
@@ -105,7 +110,7 @@ export class PlayComponent {
   }
 
   nextFood() {
-    this.selectRandomFood();
+    this.selectNextFood();
   }
 
   getGuessIcon(guess: number): string {
@@ -115,6 +120,7 @@ export class PlayComponent {
     const percentageDiff = (difference / this.currentFood.calories) * 100;
 
     if (percentageDiff <= 5) return 'check';
+    else if (percentageDiff <= 20)  return guess < this.currentFood.calories ? 'arrow-up-close' : 'arrow-down-close';
     return guess < this.currentFood.calories ? 'arrow-up' : 'arrow-down';
   }
 
@@ -123,8 +129,10 @@ export class PlayComponent {
       case 'check':
         return 'M5 13l4 4L19 7';
       case 'arrow-up':
+      case 'arrow-up-close':
         return 'M5 10l7-7m0 0l7 7m-7-7v18';
       case 'arrow-down':
+      case 'arrow-down-close':
         return 'M19 14l-7 7m0 0l-7-7m7 7V3';
       default:
         return 'M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z';
